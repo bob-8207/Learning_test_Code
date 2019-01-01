4cc20d76417d9f159704af43e26ecf4da85d4d43
bob-8207 <bob_8207@hotmail.com>
2019-01-02 01:27:49 +0800

    20190102

---
+++ backup_ver1.py
@@ -0,0 +1,38 @@
+# -*- coding:UTF-8 -*-
+import os
+import time
+
+
+#1、需要备份的文件目录与目录将被指定在一个列表中。
+#例如在：Windows系统下：
+source = ['C:\\Backup_1']
+
+#又例如在 Mac OS X 与  Linux 下：
+#source = ['/Users/swa/notes']
+
+#2、备份文件必须存储在一个主备份目录中
+#例如在：Windows系统下：
+target_dir = 'C:\\Backup_test'
+
+#又例如在 Mac OS X 与 Linux 下：
+#target_djir = '/Users/swa/backup'
+
+#3、备份文件将打包压缩成zip文件。
+#4、zip压缩文件的名由当前日期与时间构建而成。
+target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+
+#如果目标目录还不存在，则进行创建
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+#5、我们使用zip命令将文件打包成zip格式
+zip_command = 'zip -r {0} {1}'.format(target,' '.join(source))
+
+# 运行备份
+print('Zip command is: ')
+print(zip_command)
+print('Running:')
+if os.system(zip_command) == 0:
+    print('Successfl backup to', target)
+else:
+    print('Bacuup FAILED !!!')

07db24384bb5202cac70772f1935f8753bce066b
bob-8207 <bob_8207@hotmail.com>
2019-01-03 13:30:37 +0800

    201801031330

--- backup_ver2.py
+++ backup_ver2.py
@@ -7,4 +7,30 @@ import time
 #例如在：Windows系统下：
 #source = ['"C:\\My Documents"', 'C:\\Code']
 
-print('1')
+source = ['/Users/user/Desktop/backup-test/source']
+
+target_dir = '/Users/user/Desktop/backup-test/target'
+
+if not os.path.exists(target_dir):
+    os.mkdir(target_dir)
+
+today = target_dir + os.sep + time.strftime('%Y%m%d')
+
+now = time.strftime('%H%M%S')
+
+target = today + os.sep + now + '.zip'
+
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successfully created directory', today)
+
+zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
+
+print('Zip command is:')
+print(zip_command)
+print('Running:>>>>>>')
+if os.system(zip_command) == 0:
+    print('Successfully backup to', target)
+else:
+    print('Backup FAILED!!!')
+

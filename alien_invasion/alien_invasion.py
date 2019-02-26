ebe0f7e9e9ce4dddf736e339181f84d60b3227d0
bob-8207 <bob_8207@hotmail.com>
2019-02-27 00:00:48 +0800

    201902270000-H

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,9 @@
 # -*- coding:utf-8 -*-
 
-import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     #初始化游戏并创建一个屏幕对象
@@ -10,19 +11,14 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-#    bg_color = (230, 230, 230)                          #设置背景颜色
+    ship = Ship(screen)
 
     #开始游戏主循环
     while True:
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
+        gf.check_events(ship)
+        ship.update()
         #每次循环时都重绘屏幕
-        screen.fill(ai_settings.bg_color)
-
-        #让最近绘制的屏幕可见
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()

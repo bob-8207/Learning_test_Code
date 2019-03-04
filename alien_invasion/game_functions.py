5020424725de4f466ea340c0532ff6c2464327f6
bob-8207 <bob_8207@hotmail.com>
2019-03-04 23:21:39 +0800

    201903042321-H

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -3,6 +3,18 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def chenk_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """响应鼠标和键盘事件"""
     for event in pygame.event.get():
@@ -10,23 +22,18 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_lift = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_lift = False
+            chenk_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
-    """更新屏幕上的图像，并切换到新屏幕"""
+    #更新屏幕上的图像，并切换到新屏幕
     #每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
     ship.blitem()
 
     #让最近绘制的屏幕可见
     pygame.display.flip()
+    
